30e35fd6a34bd1da3fe4b23bdf8cb5b89a95e353
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-16 22:28:35 -0600

    creating display and click listner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+})
