b1a0344393bfc028fe68587db4ff87651cdf2560
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-17 19:49:15 -0600

    adding in notes and finishing js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,27 @@ var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
+
+
+    //savebtn "click" listener
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
+        //saves test to local storage
         localStorage.setItem(time, text);
     })
 
     function timeTracker(){
+
+        //gets current number of hours
         var timeNow = moment().hour();
+
+        //looping over time block
         $(".time-block").each(function (){
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -32,6 +42,8 @@ $(document).ready(function () {
             }
         })
     }
+
+    //pulls item from the local storage if there is any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
