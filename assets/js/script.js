6670199c5c7e069c83c47929da63606b917aa50f
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-17 18:43:37 -0600

    adding in display for date and time and classes for background ind

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+//Displaying todays day as well as date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
@@ -9,4 +11,27 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     })
 
+    function timeTracker(){
+        var timeNow = moment().hour();
+        $(".time-block").each(function (){
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    
 })
